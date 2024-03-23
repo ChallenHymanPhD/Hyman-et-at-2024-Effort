fe1ec68d433adf0b5530442f65edc3d2a660e524
ChallenHymanPhD <153224292+ChallenHymanPhD@users.noreply.github.com>
2024-03-23 12:37:16 -0400

    Add files via upload

---
+++ Fishable_days_03_24.R
@@ -0,0 +1,64 @@
+
+library(rnoaa)
+library(tidyverse)
+# Suppress summarise info
+options(dplyr.summarise.inform = FALSE)
+
+
+## List of buoys desired for analysis
+Buoys <- c("42012",
+           "PCLF1",
+           "PCBF1",
+           "APCF1",
+           "SGOF1",
+           "SHPF1",
+           "KTNF1",
+           "CKYF1",
+           "ARPF1",
+           "FHPF1",
+           "CWBF1",
+           "42098",
+           "42013",
+           "VENF1",
+           "BCGF1",
+           "NPSF1")
+
+Region <- c(1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2)
+SRFS <- c(1,1,1,1,1,2,2,2,2,3,3,3,3,3,3,3)
+Full_Data <- NULL
+for(j in 1:length(Buoys)){
+  my_buoy <- Buoys[j]
+  for (i in 2004:2023){
+    assign("data", try(buoy(dataset = "stdmet", buoyid = my_buoy, year = i)$data, silent = T)) ## ignores empty data values
+    if(class(data)[1] == "try-error"){
+      next
+    } else {
+      data <- as.data.frame(data[,c(1,2,3,5,7)]) ## date, lon, lat, wind speed, and wave height
+      data$Date <- as.Date(data$time)
+      data <- data%>%group_by(Date, lon, lat)%>% ## aggregate to day
+        summarize(speed = mean(wind_spd, na.rm = T),
+                  height = mean(wave_height, na.rm = T))
+      data$Buoy <- my_buoy
+      data$Region <- Region[j]
+      data$SRFS <- SRFS[j]
+      Full_Data <- rbind(Full_Data,data) ## bind to dataset
+    }
+    print(paste("Buoy", my_buoy, "in year", i, "is complete"))
+  }
+}
+
+write.csv(Full_Data, "Fishable_days_all_stations_03_24.csv")
+
+Fishable <- Full_Data%>%group_by(Date, Region)%>%
+  summarize(Speed = mean(speed, na.rm = T),
+            Height = mean(height, na.rm = T))
+
+write.csv(Fishable, "Fishable_days_MRIP_03_24.csv")
+
+
+Fishable_SRFS <- Full_Data%>%group_by(Date, SRFS)%>%
+  summarize(Speed = mean(speed, na.rm = T),
+            Height = mean(height, na.rm = T))
+
+write.csv(Fishable_SRFS, "Fishable_days_SRFS_03_24.csv")
+
